eb69f53491a8061f359caab487487a14041c89c1
owolfdev <owolfdev@gmail.com>
2023-04-30 07:07:50 +0700

    update add todo

--- lib/sendSlack.ts
+++ lib/sendSlack.ts
@@ -1,18 +1,15 @@
-// lib/slack.ts
 import { WebClient } from "@slack/web-api";
 
-const sendSlackMessage = async (channel: string, text: string) => {
-  const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
+const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
 
+export async function sendSlackMessage(channel: string, text: string) {
   try {
     await slackClient.chat.postMessage({
       channel,
       text,
     });
-    console.log("Slack message sent to:", channel);
+    console.log("Slack message sent successfully");
   } catch (error) {
-    console.error("Error sending Slack message:", error);
+    console.error("Error sending Slack message: ", error);
   }
-};
-
-export { sendSlackMessage };
+}
